5ebb08a2085cee4d4ee2776abcb4040c0140707d
Martin <MartinGlyer@gmail.com>
2018-11-22 12:09:54 -0500

    more schema

--- server/db/models.js
+++ server/db/models.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose')
 
-let User = mongoose.Schema({
+let UserSchema = mongoose.Schema({
   name: String,
-  password: String
+  password: String,
+  secretSantaID: Array
 })
+
+let SecretSantaSchema = mongoose.Schema({
+  ID: String,
+  startDate: String,
+  endDate: String
+})
+
+let User = mongoose.model('User', UserSchema)
+
+
+module.exports = {
+  User
+}
